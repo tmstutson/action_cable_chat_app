b041e9b84f5dda7ab8b51d2d89e2cc7ddd41e976
Tia Stutson <tmstutson@gmail.com>
2020-02-10 21:02:34 +0000

    fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  root 'expenses#index'
 
   resources :users
   resources :messages
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  root 'home_page#index'
+  
+  resources :expenses
+  resources :home_page 
   
   mount ActionCable.server, at: '/cable'
 end
